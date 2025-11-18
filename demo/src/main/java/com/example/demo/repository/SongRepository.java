3e18daae6e48f61580636ecfc06d01f02efd7340
youdeajin <haminw100100@gmail.com>
2025-11-18 17:28:15 +0900

    불필요한 백엔드 코드(demo 폴더) 및 SQL 파일 제거

--- demo/src/main/java/com/example/demo/repository/SongRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Song;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SongRepository extends JpaRepository<Song, Long> {
-    // JpaRepository를 상속받으면 findById(), findAll() 등의 메서드를 자동으로 사용할 수 있습니다.
-}
